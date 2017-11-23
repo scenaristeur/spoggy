bcf8b7780670771c8e1c6916913a157d622b7448
David Faveris <scenaristeur@gmail.com>
2017-11-23 12:41:58 +0100

    bascule chat

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [Documentation](http://smag0.blogspot.fr/2017/11/avec-spoggy-prenez-des-notes-et.html)
 
-Disponible : 
+Disponible :
 
 Import : commande /i
 
@@ -11,7 +11,14 @@ Export : commande /e
 [exemples de fichiers exportés depuis spoggy](https://github.com/scenaristeur/smag0-connaissance/tree/master/meet-up_conf)
 
 
-Le code source de l'application SPOGGY sera bientôt disponible. 
+Le code source de l'application SPOGGY sera bientôt disponible.
 Pour être informé de sa sortie,
 cliquez sur "Watch" en haut à droite
 ou abonnez-vous à la newsletter du blog. [http://smag0.blogspot.fr/2017/08/smagyun-comment-acceder-aux.html?m=0](http://smag0.blogspot.fr/2017/08/smagyun-comment-acceder-aux.html?m=0)
+
+
+installation :
+```
+git clone https://github.com/scenaristeur/spoggy.git
+cd spoggy
+npm install && cd public && bower install && cd .. && node .
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Setup basic express server
+var debug = true;
+
+// DEBUT SERVEUR EXPRESS
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -6,19 +9,48 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 server.listen(port, function () {
-	console.log('Server listening at port %d', port);
+  console.log('Server listening at port %d', port);
 });
 
 // Routing
 app.use(express.static(__dirname + '/public'));
 
+// fin serveur express
+
+//DEBUT SpogLevel BDD
+var SpogTools = require('./spogtools');
+var SpogLevel = require('./spoglevel');
+var st = new SpogTools(debug);
+var sl = new SpogLevel("smagBoose1"); // name, mode (n3 or null), demo or not
+st.log(sl);
+var config = {};
+//FIN SPOG LEVEL BDD
+config = {};
+config.dbName = sl.getDb();
+config.graphListe = sl.getGraphListe();
+config.room = sl.getDefaultGraphName();
+
+
+
 //Socket.IO
-io.on('connect', function(){
-	console.log("connecte");
+io.on('connect', function(socket){
+  console.log("connecte");
+  connexion(socket);
 });
 io.on('event', function(data){
-	console.log(data);
+  console.log(data);
 });
 io.on('disconnect', function(){
-	console.log("deconnecte");
+  console.log("deconnecte");
 });
+
+st.log("ready");
+
+
+// EVENEMENTS
+function connexion(socket){
+  st.log(socket);
+  socket.emit('config', config);
+socket.join(config.room);
+socket.emit('updateroom', config.room);
+}
